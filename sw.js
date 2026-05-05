54443771932769242b63959e82ebb00053639ebc
guinarutoboruto70-cpu <guinaruto.boruto70@gmail.com>
2026-05-05 00:41:56 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,39 @@
-const CACHE_NAME = 'trataflv-v2';
-const ASSETS = [
-    './',
-    './index.html',
-    './manifest.json',
-    'https://cdn-icons-png.flaticon.com/512/1041/1041310.png'
+const CACHE_NAME = "controle-flv-v2";
+
+const APP_FILES = [
+  "/CONTROLEFLVFILIAL-18/",
+  "/CONTROLEFLVFILIAL-18/index.html",
+  "/CONTROLEFLVFILIAL-18/manifest.json",
+  "/CONTROLEFLVFILIAL-18/icons/icon-192.png",
+  "/CONTROLEFLVFILIAL-18/icons/icon-512.png"
 ];
 
-// Instalação: Salva os arquivos essenciais
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
-        })
-    );
-    self.skipWaiting();
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+  );
+  self.skipWaiting();
 });
 
-// Ativação: Limpa caches antigos se você mudar o nome da versão
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cache) => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            );
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
         })
-    );
-    self.clients.claim();
+      )
+    )
+  );
+  self.clients.claim();
 });
 
-// Fetch: Tenta buscar na internet primeiro, se estiver sem sinal, usa o Cache
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request).catch(() => {
-            return caches.match(event.request);
-        })
-    );
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
