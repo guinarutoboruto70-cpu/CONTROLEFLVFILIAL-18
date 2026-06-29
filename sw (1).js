a5c90e8ebe033c602841f7056c760dd509789964
guinarutoboruto70-cpu <guinaruto.boruto70@gmail.com>
2026-06-29 20:00:39 -0300

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,57 @@
+const CACHE_NAME = "controle-flv-v33";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return Promise.allSettled(
+        APP_FILES.map(url =>
+          cache.add(url).catch(err => {
+            console.warn("Falha ao cachear:", url, err);
+          })
+        )
+      );
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).catch(() => {
+        if (event.request.destination === "document") {
+          return caches.match("./index.html");
+        }
+      });
+    })
+  );
+});
+
+self.addEventListener("message", event => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
